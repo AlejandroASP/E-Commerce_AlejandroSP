71699f3e2e5f93cde84e9c419468164a851ff69e
AlejandroASP <alejandroasp71@gmail.com>
2025-06-05 18:49:32 +0100

    Actualizar .ts main carousel, arreglar home
    Se actualiza el .ts de el componente de main carousel.
    
    Y se rehace el componente de home, por problemas.

--- e-commerce/src/app/home/main-carousel/main-carousel.component.ts
+++ e-commerce/src/app/home/main-carousel/main-carousel.component.ts
@@ -1,28 +1,33 @@
 import { Component } from '@angular/core';
-import { homeCarouselData } from '../../Data/homeCarouselData';
+import { homeCarouselData } from '../../../Data/homeCarouselData';
 import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
+
 @Component({
   selector: 'app-main-carousel',
   standalone: true,
-  imports: [],
+  imports: [NgFor, RouterLink],
   templateUrl: './main-carousel.component.html',
   styleUrl: './main-carousel.component.css'
 })
 export class MainCarouselComponent {
   carouselData: any;
   
-  currentSlide = 0;
+  currentSlide = 0; // contador de slides
 
   ngOnInit(){
     this.carouselData = homeCarouselData
+    this.autoPlay();
   }
 
+  // encargado de mover automaticamente las slides
   autoPlay(){
     setInterval(() =>{
       this.nextSlide();
     },2000 )
   }
 
+  // encargado de mover las slides
   nextSlide(){
     this.currentSlide = (this.currentSlide + 1) % this.carouselData.length;
   }
